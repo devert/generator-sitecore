78ee5f10c74016e5908b0aa26cd0e248d8e79c63
Dennis Evert <dennisevert@gmail.com>
2015-10-10 22:29:02 -0500

    Working on getting sdn login

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 
 var generators = require('yeoman-generator');
 var chalk = require('chalk');
+var request = require('superagent');
 var yosay = require('yosay');
 
 module.exports = generators.Base.extend({
@@ -11,14 +12,47 @@ module.exports = generators.Base.extend({
   },
 
   initializing: function () {
+    this.log(yosay('\'Allo \'allo!\rLet\'s create a\r' + chalk.red('Sitecore') + ' project!'));
   },
 
   prompting: {
-    askFor: function () {
-      // var done = this.async();
+    askForSDNCredentials: function () {
+      var done = this.async();
 
-      this.log(yosay('\'Allo \'allo!\rLet\'s create a\r' + chalk.red('Sitecore') + ' project!'));
+      this.log('First, I will need your SDN credentials.');
+      this.prompt([
+        {
+          type: 'input',
+          name: 'username',
+          message: 'What is your SDN username?'
+        },
+        {
+          type: 'input',
+          name: 'password',
+          message: 'What is your SDN password?'
+        }
+      ], function (answers) {
+        this.sdnUsername = answers.username;
+        this.sdnPassword = answers.password;
+        done();
+      }.bind(this));
     }
+  },
+
+  loginToSDN: function () {
+    request
+      .post('https://sdn.sitecore.net/sdn5/misc/loginpage.aspx')
+      .type('form')
+      .send({ ctl09_emailTextBox: this.sdnUsername, ctl09_passwordTextBox: this.sdnPassword })
+      .end(function(err, res){
+       if (res.ok) {
+         console.log('yay got ' + JSON.stringify(res.body));
+       } else {
+         console.log('Oh no! error ' + res.text);
+       }
+     });
   }
+
+  //
 
 });
